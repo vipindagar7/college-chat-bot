c59df92b82318f2284686e40fba30cac3b825b7e
Vipin DAgar <vipindagar@Vipins-MacBook-Air.local>
2026-05-26 22:57:09 +0530

    add base url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-// In production, app is served under /chatbot
-// In dev, served at root /
-const basename = import.meta.env.PROD ? '/chatbot' : '/'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
+    <BrowserRouter basename="/chatbot">
+          <App />
     </BrowserRouter>
   </React.StrictMode>
 )
